f70378d494e147a558e569390b546c04bae5e360
Matheus Kirchesch <matheus@btor.com.br>
2024-06-04 20:22:08 -0300

    app importado e produtos cadastrados no banco local

--- api/src/Database.cs
+++ api/src/Database.cs
@@ -2,5 +2,12 @@ namespace ProjetoWeb;
 
 public class Database
 {
-    public List<Produto> Produtos { get; set; } = [];
+    public List<Produto> Produtos { get; set; } = [
+        new("QES#34X", 0),
+        new("ESP#23Y", 0),
+        new("SDF#43Z", 0),
+        new("WRT#32P", 0),
+        new("OXT#45O", 0),
+        new("DRT#56H", 0),
+    ];
 }
--- api/src/Produto.cs
+++ api/src/Produto.cs
@@ -2,6 +2,12 @@ namespace ProjetoWeb;
 
 public class Produto
 {
+    public Produto(string codigo, int quantidade)
+    {
+        Codigo = codigo;
+        Quantidade = quantidade;
+    }
+
     public string Codigo { get; set; } = "";
     public int Quantidade { get; set; }
 }
